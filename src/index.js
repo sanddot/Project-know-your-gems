4b371612c2942cc7b8e0618233a92d8e190f231a
sanddot <sanginta@gmail.com>
2025-12-08 17:29:59 -0800

    Added basic form handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+function generateGemAnswer(event) {
+  event.preventDefault();
+  //alert("generating gems for you!");
 
+  let gemChoises = document.querySelector("#gem-descriptions");
+  gemChoises.innerHTML =
+    "Garnet Grounding, Passion and Emotional Stability Aligns with the Fire element while providing grounding";
+}
+
+let gemSearchElement = document.querySelector("#gem-search-form");
+gemSearchElement.addEventListener("submit", generateGemAnswer);
