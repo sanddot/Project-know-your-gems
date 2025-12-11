3be70df40471565c69311f2e406aa6fb1b18385b
sanddot <sanginta@gmail.com>
2025-12-11 10:32:07 -0800

    Added loading dots

--- src/index.js
+++ src/index.js
@@ -12,15 +12,11 @@ function generateGemAnswer(event) {
   let apiKey = "t3f3af9b184481d0306edc82cbo6ff8c";
   let prompt = `User instructions: Please provide a list of gems that are recommended for zodiak sign ${zodiakSignElement.value} born in ${yearInputElement.value}`;
   let context =
-    "You are well known gemologist and have a vast knowledge in astrology. Your mission is to list 4 gems and that meet user instructions. Please list each gem inside <li> element and separate each element by <br />. Add short description for each gem. Make sure intro is short and inside <h4> element";
+    "You are well known gemologist and have a vast knowledge in astrology. Your mission is to list 3 gems and that meet user instructions. Please list each gem inside <li> element and separate each element by <br />. Add short description for each gem. Make sure intro is short and inside <h4> element";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-  //console.log("Searching info on gems");
-  //console.log(`Prompt: ${prompt}`);
 
   let gemElement = document.querySelector("#gem-descriptions");
   gemElement.classList.remove("hidden");
-  gemElement.innerHTML = `Loading.....`;
 
   axios.get(apiUrl).then(searchGems);
 }
